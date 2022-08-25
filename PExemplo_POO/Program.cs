03b0e99c86f68e85d92c9148ee7298013f29dfd5
Larissa Dinart <lari_dinart@hotmail.com>
2022-08-25 11:58:43 -0300

    Adicionar arquivos de projeto.

---
+++ PExemplo_POO/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace PExemplo_POO
+{
+    internal class Program
+    {
+        static Pessoa lerPessoa()
+        {
+            string n;
+            int i;
+
+            Console.WriteLine("Informe o nome da pessoa: ");
+            n = Console.ReadLine();
+
+            Console.WriteLine("Informe a idade: ");
+            i = int.Parse(Console.ReadLine());
+
+            Pessoa pessoa = new Pessoa()
+            {
+                Nome = n,
+                Idade = i,
+            };
+            return pessoa;
+
+            //return new Pessoa()
+            //{
+               // Nome = n,
+               // Idade = i,
+           // };
+
+        }
+
+        static void Main(string[] args)
+        {
+            Pessoa[] contatos = new Pessoa[10];
+            int opc;
+
+            do
+            {
+                Console.WriteLine("Informe a opção desejada: ");
+                Console.WriteLine("1- Cadastrar\n0-Sair");
+                opc = int.Parse(Console.ReadLine());
+            }while(opc != 0 || opc != 1);
+
+            switch (opc)
+            {
+                case 0:
+                    Console.WriteLine("Tchau");
+                    break;
+                case 1:
+                    contatos[0] = lerPessoa();
+                    break;
+                default:
+                    break;
+            }
+            Console.WriteLine(">>>>>FIM<<<<<");
+        }
+    }
+}
